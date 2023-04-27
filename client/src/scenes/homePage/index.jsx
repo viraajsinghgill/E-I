fbccb28110219cff62ce0a8b2243f933f788845a
viraajsinghgill <viraajgill2@gmail.com>
2023-04-27 12:12:23 +0530

    added vercel file

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
 
-  return (
+  return ( 
     <Box>
       <Navbar />
       <Box
